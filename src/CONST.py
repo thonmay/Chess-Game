3e64e078a424f41bf5d3b20e9fdd7dbb479e5c15
Thonmay <mdthoriqulislam384@gmail.com>
2024-12-09 21:06:16 +0100

    The game window and basic structure is being created

---
+++ src/CONST.py
@@ -0,0 +1,8 @@
+# screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+# board dimensions
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = SCREEN_WIDTH // COLS
---
+++ src/main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+from CONST import *
+
+class Main:
+    
+    def __init__(self):
+        pygame.init()
+    
+    def gameloop(self):
+        pass
+    
+# main = Main()
+# main.gameloop()
